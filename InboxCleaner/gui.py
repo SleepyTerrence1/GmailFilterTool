6868da5790ed0747b81b74e4efae9d8d0d30f54e
Terrence <liuterrence4@gmail.com>
2025-12-13 20:27:01 +0000

    Fixed gui.py

--- InboxCleaner/gui.py
+++ InboxCleaner/gui.py
@@ -2,15 +2,16 @@ import tkinter as tk
 from main import gmail_login, load_filters
 
 def run_app():
-    service = gmail_login()
-    filters = load_filters()
-
     root = tk.Tk()
     root.title("Inbox Cleaner(Gmail)")
     root.geometry("500x400")
 
+    tk.Label(root, text="Inbox Cleaner").pack()
 
+    filters = load_filters()
+    tk.Label(root, text="Loaded Filters:").pack()
 
+    tk.Button(root, text="Apply Filters", command=lambda: print("Applying filters...") )
 
 
     root.mainLoop()
